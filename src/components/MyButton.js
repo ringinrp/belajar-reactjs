49b7ea7a6e8effc993d21c415f03ecb0cfe582da
Ringin Restu Pati <ringinrp21@gmail.com>
2023-01-14 11:04:34 +0700

    first commit

---
+++ src/components/MyButton.js
@@ -0,0 +1,8 @@
+const MyButton = () => {
+    return(
+        <div>
+            <button>Click Me</button>
+        </div>
+    )
+}
+export default MyButton;
---
+++ src/elements/button/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function button(propps) {
+  return (
+    <div>indesx</div>
+  )
+}
